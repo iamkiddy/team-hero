01e466aa4f18ce3988da631526ae16bf49d7ab4a
iamkiddy <francisyoungson@gmail.com>
2020-08-09 01:09:16 +0800

    initialized the  Api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// imorting Axios Library
+import axios from "axios"
+
 // importing search component
 import Search from "./Search/Search";
 
 class App extends Component {
+  // setting states 
+  state = {
+    //Api key
+    API_KEY: "ef1161f79e50c9217501e4052c5fbf73",
+    city: "",
+    country: "",
+    icon: "",
+    temp:"",
+    pressure:"",
+    humidity:"",
+  }
+
+  
+  //initialising  the Api
+
+  getWeather(){
+    axios.get(`api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.API_KEY}`)
+    .then((response)=>{
+      this.state.temp = response.data.main.temp;
+      this.state.pressure = response.data.main.pressure;
+      this.state.humidity = response.data.main.humidity;
+    })
+  }
   render () {
     return (
       <div className="container-lg">
--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+
   render () {
     return (
       <div>
         <div class="flex flex-col items-center justify-center border-b border-gray-300 p-3 shadow rounded text-gray-600">
-          <input type="search" name="search" placeholder="Search" class="h-10 px-5 pr-10 rounded-full text-sm focus:outline-none shadow-inner bg-gray-200 w-2/5" />
-          <button class="bg-gray-200 text-sm text-gray-600 hover:bg-gray-300 mt-4 border border-gray-400 border-b-2 text-black font-bold py-1 px-8 shadow rounded-lg hover:border-b focus:bg-gray-300 focus:shadow-inner focus:outline-none hover:text-sm">
+          <input type="search" name="search"   placeholder="Search" class="h-10 px-5 pr-10 rounded-full text-sm focus:outline-none shadow-inner bg-gray-200 w-2/5" />
+          <button  class="bg-gray-200 text-sm text-gray-600 hover:bg-gray-300 mt-4 border border-gray-400 border-b-2 text-black font-bold py-1 px-8 shadow rounded-lg hover:border-b focus:bg-gray-300 focus:shadow-inner focus:outline-none hover:text-sm">
             Search
           </button>
         </div>
       </div>
     )
   }
+  
 }
 
 export default Search;
